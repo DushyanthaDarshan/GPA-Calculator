fcd021613451988393c2d268c77fe9eddb0117c1
Dushyantha <dushyantha1208@gmail.com>
2021-09-29 15:45:33 +0530

    prediction page interface

--- src/main/java/com/TeamPhoenix/gpaCalculator/service/manager/PredictionPage.java
+++ src/main/java/com/TeamPhoenix/gpaCalculator/service/manager/PredictionPage.java
@@ -7,8 +7,11 @@ import com.TeamPhoenix.gpaCalculator.beans.User;
 import com.TeamPhoenix.gpaCalculator.service.dao.GpaCalDao;
 import com.TeamPhoenix.gpaCalculator.service.dao.Impl.GpaCalDaoImpl;
 import com.google.gson.Gson;
+import org.apache.commons.collections4.CollectionUtils;
 
 import javax.swing.*;
+import javax.swing.border.TitledBorder;
+import javax.swing.table.*;
 import java.awt.*;
 import java.util.*;
 import java.util.List;
@@ -20,6 +23,10 @@ public class PredictionPage {
 //    private List<Double> gpvList = Arrays.asList(4.0, 4.0, 3.7, 3.3, 3.0, 2.7, 2.4, 2.1);
     private Map<String, Double> gradeWithGpvMap = new HashMap<>();
     private JFrame frame;
+    private JTable table;
+    private DefaultTableModel model;
+    private Long userId;
+    private Integer semNumber;
 
     /**
      * Launch the application.
@@ -28,7 +35,7 @@ public class PredictionPage {
         EventQueue.invokeLater(new Runnable() {
             public void run() {
                 try {
-                    PredictionPage window = new PredictionPage();
+                    PredictionPage window = new PredictionPage(1l);
                     window.frame.setVisible(true);
                 } catch (Exception e) {
                     e.printStackTrace();
@@ -40,7 +47,8 @@ public class PredictionPage {
     /**
      * Create the application.
      */
-    public PredictionPage() {
+    public PredictionPage(Long userId) {
+        this.userId = userId;
         initialize();
     }
 
@@ -53,6 +61,7 @@ public class PredictionPage {
         frame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
         frame.setVisible(true);
         frame.getContentPane().setLayout(null);
+        frame.setResizable(false);
 
         JPanel panel = new JPanel();
         panel.setBounds(0, 0, 1000, 750);
@@ -64,7 +73,58 @@ public class PredictionPage {
         panel.add(panel_1);
         panel_1.setLayout(null);
 
+        model = new DefaultTableModel();
+        model.addColumn("Course Unit");
+        model.addColumn("Course Name");
+        model.addColumn("First Class");
+        model.addColumn("Second Upper Class");
+        model.addColumn("Second Lower Class");
+        model.addColumn("General Normal Class");
+
         populatePrediction();
+
+        JLabel semesterNumberLabel = new JLabel("Prediction for Semester " + semNumber);
+        semesterNumberLabel.setFont(new Font("Tahoma", Font.BOLD, 25));
+        semesterNumberLabel.setBounds(359, 13, 500, 40);
+        semesterNumberLabel.setForeground(new Color(255, 255, 255));
+        semesterNumberLabel.setBackground(new Color(255, 255, 255));
+        panel_1.add(semesterNumberLabel);
+
+        JPanel panelForTable = new JPanel();
+        panelForTable.setBorder(BorderFactory.createTitledBorder( BorderFactory.createEtchedBorder(), "Courses with Predict Results", TitledBorder.CENTER, TitledBorder.TOP));
+        panelForTable.setBounds(50, 100, 900, 500);
+
+        table = new JTable(model);
+        TableColumnModel columnModel = table.getColumnModel();
+        table.setRowHeight(30);
+        columnModel.getColumn(0).setPreferredWidth(200);
+        columnModel.getColumn(1).setPreferredWidth(400);
+        columnModel.getColumn(2).setPreferredWidth(200);
+        columnModel.getColumn(3).setPreferredWidth(200);
+        columnModel.getColumn(4).setPreferredWidth(200);
+        columnModel.getColumn(5).setPreferredWidth(200);
+
+        JTableHeader header = table.getTableHeader();
+        header.setPreferredSize(new Dimension(100, 50));
+
+        DefaultTableCellRenderer centerRenderer = new DefaultTableCellRenderer();
+        centerRenderer.setHorizontalAlignment( JLabel.CENTER );
+        table.getColumnModel().getColumn(0).setCellRenderer( centerRenderer );
+        table.getColumnModel().getColumn(2).setCellRenderer( centerRenderer );
+        table.getColumnModel().getColumn(3).setCellRenderer( centerRenderer );
+        table.getColumnModel().getColumn(4).setCellRenderer( centerRenderer );
+        table.getColumnModel().getColumn(5).setCellRenderer( centerRenderer );
+        table.setSize(800, 500);
+        table.setPreferredScrollableViewportSize(table.getSize());
+        table.setFillsViewportHeight(true);
+        panelForTable.add(new JScrollPane(table));
+        panel_1.add(panelForTable);
+
+        JLabel rightSideBackground = new JLabel("");
+        rightSideBackground.setIcon(new ImageIcon("src/main/java/com/TeamPhoenix/gpaCalculator/service/imgs/right_side_signup_page.jpg"));
+        rightSideBackground.setHorizontalAlignment(SwingConstants.CENTER);
+        rightSideBackground.setBounds(0, 0, 1000, 750);
+        panel_1.add(rightSideBackground);
     }
 
     private void populatePrediction () {
@@ -78,9 +138,7 @@ public class PredictionPage {
         gradeWithGpvMap.put("C", 2.1);
 
         GpaCalDao gpaCalDao = new GpaCalDaoImpl();
-        User userFromDb = gpaCalDao.getUserDetailsByUsername("dushyantha1208@gmail.com");
-        if (userFromDb != null) {
-            Long userId = userFromDb.getUserId();
+        if (userId != null) {
             List<Gpa> gpaListFromDb = gpaCalDao.getAllGpaByUserId(userId);
             List<Integer> semNumbersList = new ArrayList<>();
             for (Gpa gpa : gpaListFromDb) {
@@ -91,7 +149,8 @@ public class PredictionPage {
                 }
             }
             int sizeOfList = semNumbersList.size();
-            User userWithNextSemSubjects = gpaCalDao.getAllSubjectAndUserDetailsBySemNumber(sizeOfList, userId);
+            semNumber = sizeOfList + 1;
+            User userWithNextSemSubjects = gpaCalDao.getAllSubjectAndUserDetailsBySemNumber(sizeOfList + 1, userId);
             if (userWithNextSemSubjects != null) {
 //                Gpa overallGpa = gpaCalDao.getOverallGpa(userId, "OVERALL");
                 List<PredictReportResult> oldSemResults = new ArrayList<>();
@@ -126,7 +185,11 @@ public class PredictionPage {
                 List<PredictReportResult> secondLowerList = new ArrayList<>();
                 List<PredictReportResult> secondUpperList = new ArrayList<>();
                 List<PredictReportResult> firstClassList = new ArrayList<>();
+                Map<String, List<PredictReportResult>> classesMap = new HashMap<>();
                 boolean isFirstClassFound = false;
+                boolean isSecondUpperClassFound = false;
+                boolean isSecondLowerClassFound = false;
+                boolean isThirdUpperClassFound = false;
                 while (!isFirstClassFound) {
                     for (PredictReportResult predictReportResult : newSemResults) {
                         if (predictReportResult.getResultGrade().equals("C")) {
@@ -149,14 +212,50 @@ public class PredictionPage {
                             predictReportResult.setGpv(gradeWithGpvMap.get("A"));
                         }
                         double newGpa = calculateGpa(oldSemResults, newSemResults);
-                        if (newGpa >= 2.7 && newGpa < 3.0) {
-                            thirdUpperList.add(predictReportResult);
+                        if (newGpa < 3.0) {
+                            if (!isThirdUpperClassFound) {
+                                for (PredictReportResult predictReportResult1 : newSemResults) {
+                                    PredictReportResult predictReportResult2 = new PredictReportResult();
+                                    predictReportResult2.setSubjectName(predictReportResult1.getSubjectName());
+                                    predictReportResult2.setSubjectCode(predictReportResult1.getSubjectCode());
+                                    predictReportResult2.setResultGrade(predictReportResult1.getResultGrade());
+                                    predictReportResult2.setSubjectCredit(predictReportResult1.getSubjectCredit());
+                                    predictReportResult2.setGpv(predictReportResult1.getGpv());
+                                    predictReportResult2.setNewSem(false);
+                                    thirdUpperList.add(predictReportResult2);
+                                }
+                                isThirdUpperClassFound = true;
+                            }
                         } else if (newGpa >= 3.0 && newGpa < 3.3) {
-                            secondLowerList.add(predictReportResult);
+                            if (!isSecondLowerClassFound) {
+                                for (PredictReportResult predictReportResult1 : newSemResults) {
+                                    PredictReportResult predictReportResult2 = new PredictReportResult();
+                                    predictReportResult2.setSubjectName(predictReportResult1.getSubjectName());
+                                    predictReportResult2.setSubjectCode(predictReportResult1.getSubjectCode());
+                                    predictReportResult2.setResultGrade(predictReportResult1.getResultGrade());
+                                    predictReportResult2.setSubjectCredit(predictReportResult1.getSubjectCredit());
+                                    predictReportResult2.setGpv(predictReportResult1.getGpv());
+                                    predictReportResult2.setNewSem(false);
+                                    secondLowerList.add(predictReportResult2);
+                                }
+                                isSecondLowerClassFound = true;
+                            }
                         } else if (newGpa >= 3.3 && newGpa < 3.7) {
-                            secondUpperList.add(predictReportResult);
+                            if (!isSecondUpperClassFound) {
+                                for (PredictReportResult predictReportResult1 : newSemResults) {
+                                    PredictReportResult predictReportResult2 = new PredictReportResult();
+                                    predictReportResult2.setSubjectName(predictReportResult1.getSubjectName());
+                                    predictReportResult2.setSubjectCode(predictReportResult1.getSubjectCode());
+                                    predictReportResult2.setResultGrade(predictReportResult1.getResultGrade());
+                                    predictReportResult2.setSubjectCredit(predictReportResult1.getSubjectCredit());
+                                    predictReportResult2.setGpv(predictReportResult1.getGpv());
+                                    predictReportResult2.setNewSem(false);
+                                    secondUpperList.add(predictReportResult2);
+                                }
+                                isSecondUpperClassFound = true;
+                            }
                         } else if (newGpa >= 3.7) {
-                            firstClassList.add(predictReportResult);
+                            firstClassList.addAll(newSemResults);
                             isFirstClassFound = true;
                             break;
                         }
@@ -166,6 +265,41 @@ public class PredictionPage {
                 System.out.println("secondLowerList"  + new Gson().toJson(secondLowerList));
                 System.out.println("secondUpperList" + new Gson().toJson(secondUpperList));
                 System.out.println("firstClassList" + new Gson().toJson(firstClassList));
+
+                for (PredictReportResult predictReportResult : firstClassList) {
+                    if (predictReportResult != null) {
+                        String subjectCode = predictReportResult.getSubjectCode();
+                        String subjectName = predictReportResult.getSubjectName();
+                        Integer subjectCredit = predictReportResult.getSubjectCredit();
+                        String firstClassResultGrade = predictReportResult.getResultGrade();
+                        String secondUpperClassResultGrade = predictReportResult.getResultGrade();
+                        String secondLowerClassResultGrade = predictReportResult.getResultGrade();
+                        String generalBelowClassResultGrade = predictReportResult.getResultGrade();
+                        for (PredictReportResult predictReportResult2ndUpperClass : secondUpperList) {
+                            if (predictReportResult2ndUpperClass != null) {
+                                if (predictReportResult2ndUpperClass.getSubjectCode().equals(subjectCode)) {
+                                    secondUpperClassResultGrade = predictReportResult2ndUpperClass.getResultGrade();
+                                }
+                            }
+                        }
+                        for (PredictReportResult predictReportResult2ndLowerClass : secondLowerList) {
+                            if (predictReportResult2ndLowerClass != null) {
+                                if (predictReportResult2ndLowerClass.getSubjectCode().equals(subjectCode)) {
+                                    secondLowerClassResultGrade = predictReportResult2ndLowerClass.getResultGrade();
+                                }
+                            }
+                        }
+                        for (PredictReportResult predictReportResultGeneralBelowClass : thirdUpperList) {
+                            if (predictReportResultGeneralBelowClass != null) {
+                                if (predictReportResultGeneralBelowClass.getSubjectCode().equals(subjectCode)) {
+                                    generalBelowClassResultGrade = predictReportResultGeneralBelowClass.getResultGrade();
+                                }
+                            }
+                        }
+                        model.addRow(new Object[]{subjectCode, subjectName, firstClassResultGrade, secondUpperClassResultGrade,
+                            secondLowerClassResultGrade, generalBelowClassResultGrade});
+                    }
+                }
             }
         }
     }
